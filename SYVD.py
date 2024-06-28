f18aa359974034f6101318c872474176a0e9617f
Unknown_32 <70798935+UnknownUsr32@users.noreply.github.com>
2024-06-28 23:44:27 +0700

    Update SYVD.py
    User can now choose the resolution,Error handling,Bug fixes

--- SYVD.py
+++ SYVD.py
@@ -1,21 +1,39 @@
 from pytube import YouTube
 import os
 from moviepy.editor import VideoFileClip, AudioFileClip
+import time
 
 def download_streams(youtube_obj, output_path, filename):
-    video_stream = youtube_obj.streams.filter(adaptive=True, only_video=True).order_by('resolution').desc().first()
+    video_stream = youtube_obj.streams.filter(adaptive=True, only_video=True).order_by('resolution').desc()
     audio_stream = youtube_obj.streams.filter(adaptive=True, only_audio=True).order_by('abr').desc().first()
-    if not filename:
-        filename = video_stream.title
-        filename.strip().replace(" ", "_")
 
-    video_path = os.path.join(output_path, f"{filename}_video.mp4")
-    audio_path = os.path.join(output_path, f"{filename}_audio.mp4")
+    seen_resolution = {}
+    unique_video_stream = []
+    for stream in video_stream:
+        if stream.resolution not in seen_resolution:
+            seen_resolution[stream.resolution] = True
+            unique_video_stream.append(stream)
 
-    video_stream.download(output_path=output_path, filename=f"{filename}_video.mp4")
-    audio_stream.download(output_path=output_path, filename=f"{filename}_audio.mp4")
+    print("Available Resolutions: ")
+    for i, stream in enumerate(unique_video_stream):
+        print(f"{i+1}. {stream.resolution}")
 
-    return video_path, audio_path, os.path.join(output_path, f"{filename}.mp4")
+    q3 = int(input("Choose the resolution number: ")) - 1
+    if q3 < 0 or q3 >= len(unique_video_stream):
+        print("Invalid selection")
+        return None, None, None
+
+    video_stream = video_stream[q3]
+    if video_stream and audio_stream:
+
+        video_path = os.path.join(output_path, f"{filename}_video.mp4")
+        audio_path = os.path.join(output_path, f"{filename}_audio.mp4")
+
+        video_stream.download(output_path=output_path, filename=f"{filename}_video.mp4")
+        audio_stream.download(output_path=output_path, filename=f"{filename}_audio.mp4")
+
+        return video_path, audio_path, os.path.join(output_path, f"{filename}.mp4")
+
 
 def merge(video_path, audio_path, output_path):
     video_clip = VideoFileClip(video_path)
@@ -27,6 +45,8 @@ def merge(video_path, audio_path, output_path):
     os.remove(video_path)
     os.remove(audio_path)
     print("Video downloaded and merged!")
+    input('Press enter to continue...')
+
 
 def Download(link, output_path, filename):
     youtubeObj = YouTube(link)
@@ -35,7 +55,7 @@ def Download(link, output_path, filename):
     try:
         if choose.lower() == 'y':
             print("Fetching audio stream...")
-            stream = youtubeObj.streams.filter(only_audio=True).first()
+            stream = youtubeObj.streams.filter(only_audio=True).order_by('abr').desc().first()
             print("Downloading audio...")
             out_file = stream.download(output_path=output_path, filename=filename)
 
@@ -45,23 +65,40 @@ def Download(link, output_path, filename):
             print("Audio file downloaded!")
         else:
             q2 = '''Choose download mode:
-    1.Adaptive Mode(Recommended)
-    2.Progressive Mode(Terrible quality)'''
+1.Adaptive Mode(High Quality,Resource Heavy)
+2.Progressive Mode(Terrible Quality,Faster)'''
             print(q2)
             selection = input("===>").strip().lower()
             if selection == '1':
-                print('Fetching highest resolution video stream...')
+                print('Fetching video stream...')
                 video_path, audio_path, output_path = download_streams(youtubeObj, output_path, filename)
                 merge(video_path, audio_path, output_path)
             elif selection == '2':
-                print('Fetching highest resolution video stream...')
-                video_stream = youtubeObj.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
+                print('Fetching video stream...')
+                video_stream = youtubeObj.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc()
+                seen_resolution = {}
+                unique_video_stream = []
+                for stream in video_stream:
+                    if stream.resolution not in seen_resolution:
+                        seen_resolution[stream.resolution] = True
+                        unique_video_stream.append(stream)
+
+                print("Available Resolutions: ")
+                for i, stream in enumerate(unique_video_stream):
+                    print(f"{i + 1}. {stream.resolution}")
+
+                q3 = int(input("Choose the resolution number: ")) - 1
+                if q3 < 0 or q3 >= len(unique_video_stream):
+                    print("Invalid selection")
+                    return None, None, None
+
+                video_stream = video_stream[q3]
                 if video_stream:
                     print('Downloading video...')
-                    video_stream.download(output_path=output_path, filename=filename)
+                    video_stream.download(output_path=output_path, filename=filename + '.mp4')
                     print("Video downloaded!")
             else:
-                print('Invalid selection!')
+                print('Invalid input!')
                 return
 
     except Exception as e:
@@ -90,8 +127,10 @@ def main():
                      `--`             '---'  '''
     bold = '\033[1m'
     reset = '\033[0m'
+    clear = lambda: print('\033c', end='', flush=True)
     t1 = "       Simple Youtube Video Downloader"
     t2 = "                By Unknown_32"
+    clear()
     print(ascii)
     print(f'{bold}{t1}{reset}')
     print(f'{bold}{t2}{reset}')
@@ -100,6 +139,9 @@ def main():
         if not link:
             print("URL cannot be empty")
             return
+        validation = YouTube(link)
+        clear()
+        print(ascii)
         path = input("Enter path to which the downloaded file will be saved(Press enter to save on current directory): ").strip()
         if not path:
             path = os.getcwd()
@@ -117,10 +159,56 @@ def main():
         if not check_path_access(path):
             return
 
-        name = input("Enter output name(Press enter for default name): ")
+
+        clear()
+        print(ascii)
+        name = input("Enter output name(Press enter for default name): ").strip().replace(' ', '_')
         if not name:
             name = None
+        if not name:
+            name = validation.title.strip().replace(' ', '_')
+        print(path + name + '.mp4')
+        if os.name == 'nt':
+            if os.path.isfile(path + r'\\' + name + '.mp4'):
+                print("File already exists")
+                time.sleep(1.5)
+                clear()
+                print(ascii)
+                print('''
+1.Replace
+2.Rename
+3.Exit''')
+                Action = input('===>')
+                if Action == '1':
+                    os.remove(path + r'\\' + name + '.mp4')
+                elif Action == '2':
+                    path = input("Enter path to which the downloaded file will be saved(Press enter to save on current directory): ").strip()
+                elif Action == '3':
+                    return
+                else:
+                    print('Invalid input')
+        else:
+            if os.path.isfile(path + r'/' + name + '.mp4'):
+                print("File already exists")
+                time.sleep(1.5)
+                clear()
+                print(ascii)
+                print('''
+1.Replace
+2.Rename
+3.Exit''')
+                Action = input('===>')
+                if Action == '1':
+                    os.remove(path + r'/' + name + '.mp4')
+                elif Action == '2':
+                    path = input("Enter path to which the downloaded file will be saved(Press enter to save on current directory): ").strip()
+                elif Action == '3':
+                    return
+                else:
+                    print('Invalid input')
 
+        clear()
+        print(ascii)
         Download(link, path, name)
     except KeyboardInterrupt as a:
         print(f'\nProcess interrupted by user. Exiting...{a}')
